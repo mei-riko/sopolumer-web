dd705b5be5b0b73cd561ca52d6048700feb95ea8
mei-riko <dobrovolska.marina@gmail.com>
2019-08-16 15:09:58 +0300

    check work

--- public/js/app.js
+++ public/js/app.js
@@ -76,7 +76,49 @@ var _jquery2 = _interopRequireDefault(_jquery);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-(0, _jquery2.default)(document).ready(function () {});
+(0, _jquery2.default)(document).ready(function () {
+    (0, _jquery2.default)("a.scroll").click(function () {
+        (0, _jquery2.default)("html, body").animate({
+            scrollTop: (0, _jquery2.default)((0, _jquery2.default)(this).attr("href")).offset().top + "px"
+        }, {
+            duration: 500,
+            easing: "swing"
+        });
+        return false;
+    });
+
+    // Input mask
+    if ((0, _jquery2.default)('.phone').length > 0) {
+        (0, _jquery2.default)(".phone").inputmask({
+            mask: "8 999 999 99 99",
+            placeholder: " ",
+            showMaskOnHover: true,
+            onincomplete: function onincomplete() {
+                (0, _jquery2.default)(this).closest("form").addClass('error-phone');
+                (0, _jquery2.default)(this).addClass('error');
+                (0, _jquery2.default)(this).siblings(".error_phone").addClass('error').html('Укажите корректный номер');
+            },
+            oncomplete: function oncomplete() {
+                (0, _jquery2.default)(this).closest("form").removeClass('error-phone');
+                (0, _jquery2.default)(this).removeClass('error');
+                (0, _jquery2.default)(this).siblings(".error_phone").removeClass('error').html('');
+            }
+        });
+    }
+    (0, _jquery2.default)('input.phone').on('keydown', function (event) {
+        if (event.keyCode === 13 && !(0, _jquery2.default)(this).inputmask("isComplete")) {
+            event.preventDefault();
+            (0, _jquery2.default)(this).blur();
+            return false;
+        }
+    });
+
+    // Index Slider
+    (0, _jquery2.default)('.slider-block .slider-block__content').slick({
+        slidesToShow: 4,
+        variableWidth: true
+    });
+});
 
 /***/ }),
 /* 1 */
